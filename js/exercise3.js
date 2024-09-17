1f1995f2a77813db9a8ec32afc317a40c6093a69
Julia_Chu <z3211232001@gmail.com>
2024-09-17 11:52:25 -0700

    creat exercise3.js

---
+++ js/exercise3.js
@@ -0,0 +1,18 @@
+//Write an algorithm and draw a flowchart that will read three numbers and prints the value of the largest number.
+let a = 1
+let b = 3
+let c = 2
+
+if (a > b) {
+      if (a > c) {
+            console.log(`${a},a is the largest number`)
+      } else {
+            console.log(`${c},c is the largest number`)
+      }
+} if (b > a) {
+      if (b > c) {
+            console.log(`${b},b is the largest number`)
+      } else {
+            console.log(`${c},c is the largest number`)
+      }
+}

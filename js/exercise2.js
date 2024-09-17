a9e5963f2ed990db1f2ef6e49785c5741da90d87
Julia_Chu <z3211232001@gmail.com>
2024-09-17 11:34:57 -0700

    creat exercise2.js

---
+++ js/exercise2.js
@@ -0,0 +1,8 @@
+//Input the width (W) and the length (L) of a rectangle
+//Calculate the area (A) by multiplying W by L
+//Print 
+
+let width = 10
+let length = 20
+let area = width * length
+console.log(area)
